1246381d832087045a53f25b6be4947c56e4ebf4
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-04-11 00:03:53 -0300

    precisa continuar com banco de dados e tabela para matemática

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Tela1Component } from '../app/screens/tela1/tela1.component';
+import { Tela2Component } from '../app/screens/tela2/tela2.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Tela1Component,
+    Tela2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule, 
+    NgbModule, 
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
